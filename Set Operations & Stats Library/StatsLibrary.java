79853959add6be73189faaa7acbe55f8273e0a6a
wolferin12 <95312861+wolferin12@users.noreply.github.com>
2025-03-08 08:14:51 -0500

    Add files via upload

--- Set Operations & Stats Library/StatsLibrary.java
+++ Set Operations & Stats Library/StatsLibrary.java
@@ -84,6 +84,12 @@ public class StatsLibrary {
         System.out.println("Permutation: "+permutation);
         combination= Factorial(b)/(Factorial(y)*Factorial(b-y));
         System.out.println("Combination: "+combination);
+        double p= .30;
+        double q= 1-p;
+        double biDistribution= combination*Math.pow(p,y)*Math.pow(q,n-y);
+        double geoDistribution= Math.pow(q, y-1)*p;
+        System.out.println("Binomial Distribution: "+String.format("%.3f",biDistribution));
+        System.out.println("Geometric Probability Distribution: "+String.format("%.3f",geoDistribution));
 
     }
 }
